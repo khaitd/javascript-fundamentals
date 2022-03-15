e2060db32ce7ec274a33dd2bfab0245b8f1da8cb
Dmitriy <khaitdmitriy@gmail.com>
2022-03-15 15:32:13 -0400

    added index.js info

---
+++ module.js
@@ -0,0 +1,9 @@
+let myName = prompt('Enter your name')
+let month = prompt('What month were you born?')
+let food = prompt('What is your favorite food?')
+
+
+
+console.log('My name is', myName)
+console.log('I wa born in', month)
+console.log('My favorite food is', food)
