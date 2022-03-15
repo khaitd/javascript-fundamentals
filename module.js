c1c7e95453af6c6b8494e3b89e83d2b987396626
Dmitriy <khaitdmitriy@gmail.com>
2022-03-15 15:45:03 -0400

    added function

--- module.js
+++ module.js
@@ -1,9 +1,15 @@
-let myName = prompt('Enter your name')
-let month = prompt('What month were you born?')
-let food = prompt('What is your favorite food?')
+
+
+function quiz() {
+    let myName = prompt('Enter your name')
+    let month = prompt('What month were you born?')
+    let food = prompt('What is your favorite food?')
 
 
 
-console.log('My name is', myName)
-console.log('I wa born in', month)
-console.log('My favorite food is', food)
+    console.log('My name is', myName)
+    console.log('I wa born in', month)
+    console.log('My favorite food is', food)
+
+}
+
