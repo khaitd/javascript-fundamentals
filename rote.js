dc62ecebd09e6cf2023fc7e916a94b797d34212a
Dmitriy <khaitdmitriy@gmail.com>
2022-03-16 13:31:24 -0400

    added code to rote.js

---
+++ rote.js
@@ -0,0 +1,24 @@
+
+
+
+
+const greet = () => {
+    console.log("Hello world")
+
+
+}
+
+greet()
+
+const square = (num) => {
+    return num * num
+  //  return num ** 2
+}
+
+square(25)
+
+const greetByName = (str) => {
+    console.log('Hello', str)
+}
+
+greetByName("Dmitriy")
